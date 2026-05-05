13b4a8c1b7ad838c2de69df082a0e42879a8529f
Alexander Prypyshnyi <alexpryp@gmail.com>
2026-05-05 22:10:50 +0300

    ADD |  Letter component and and logic to the HighlightedLetters component

--- src/components/profile/highlightedLetters/HighlightedLetters.jsx
+++ src/components/profile/highlightedLetters/HighlightedLetters.jsx
@@ -1,16 +1,46 @@
-// import { useState } from "react";
-// import { initialLetters } from "@/components/profile/highlightedLetter/data";
+import { useState } from "react";
+import { initialLetters } from "@/components/profile/highlightedLetters/data";
+import Letter from "@/components/profile/highlightedLetters/Letter";
 import "./styles.css";
 
 export default function HighlightedLetter() {
-  // const [letters, setLetters] = useState(initialLetters);
-  // const [highlightedLetter, setHighlightedLetter] = useState(null);
+  const [letters, setLetters] = useState(initialLetters);
+  const [highlightedLetter, setHighlightedLetter] = useState(null);
+
+  function handleHover(letter) {
+    setHighlightedLetter(letter);
+  }
+
+  function handleStar(starred) {
+    setLetters(
+      letters.map((letter) => {
+        if (letter.id === starred.id) {
+          return {
+            ...letter,
+            isStarred: !letter.isStarred,
+          };
+        } else {
+          return letter;
+        }
+      }),
+    );
+  }
 
   return (
     <>
       <hr />
       <h2>Inbox</h2>
-      <ul>Letters</ul>
+      <ul>
+        {letters.map((letter) => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isHighlighted={letter === highlightedLetter}
+            onHover={handleHover}
+            onToggleStar={handleStar}
+          />
+        ))}
+      </ul>
     </>
   );
 }

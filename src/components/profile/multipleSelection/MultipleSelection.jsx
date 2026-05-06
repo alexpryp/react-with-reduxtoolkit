8e166a18e4b81c2fc63b020d946cad395325da0d
Alexander Prypyshnyi <alexpryp@gmail.com>
2026-05-06 17:44:30 +0300

    ADD | logic to the MultipleSelection component

--- src/components/profile/multipleSelection/MultipleSelection.jsx
+++ src/components/profile/multipleSelection/MultipleSelection.jsx
@@ -1,13 +1,36 @@
-// import { letters } from "./data.js";
-// import Letter from "@/components/profile/multipleSelection/Letter";
+import { useState } from "react";
+import { letters } from "@/components/profile/multipleSelection/data";
+import Letter from "@/components/profile/multipleSelection/Letter";
 
 import "./styles.css";
 
 export default function MultipleSelection() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  const selectedCount = 1;
+
+  function handleToggle(toggledId) {
+    setSelectedId(toggledId);
+  }
+
   return (
     <>
       <hr />
-      <h1>MultipleSelection</h1>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map((letter) => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isSelected={letter.id === selectedId}
+            onToggle={handleToggle}
+          />
+        ))}
+        <hr />
+        <p>
+          <b>You selected {selectedCount} letters</b>
+        </p>
+      </ul>
     </>
   );
 }

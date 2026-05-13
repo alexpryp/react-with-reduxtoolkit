28f366c19c15437fe35f688247394ff7a64b450e
Alexander Prypyshnyi <alexpryp@gmail.com>
2026-05-13 14:16:14 +0300

    ADD | StateWithReducer component

---
+++ src/components/profile/stateWithReducer/StateWithReducer.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import AddTask from "@/components/profile/stateWithReducer/AddTask";
+
+export default function StateWithReducer() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  function handleAddTask(text) {
+    setTasks([
+      ...tasks,
+      {
+        id: nextId++,
+        text: text,
+        done: false,
+      },
+    ]);
+  }
+
+  return (
+    <>
+      <hr />
+      <h1>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+    </>
+  );
+}
+
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: "Visit Kafka Museum", done: true },
+  { id: 1, text: "Watch a puppet show", done: false },
+  { id: 2, text: "Lennon Wall pic", done: false },
+];

6eca5fef89fb3a9acd932b140dc6fa357b4a6207
Alexander Prypyshnyi <alexpryp@gmail.com>
2026-05-14 23:01:30 +0300

    REF | replace the usState with a useReducer

--- src/components/profile/stateWithReducer/AddTask.jsx
+++ src/components/profile/stateWithReducer/AddTask.jsx
@@ -11,7 +11,7 @@ export default function AddTask({ onAddTask }) {
         onChange={(e) => setText(e.target.value)}
       />
       <button
-        onClick={() => {
+        onClick={(e) => {
           e.stopPropagation();
           setText("");
           onAddTask(text);
--- src/components/profile/stateWithReducer/StateWithReducer.jsx
+++ src/components/profile/stateWithReducer/StateWithReducer.jsx
@@ -1,35 +1,30 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import AddTask from "@/components/profile/stateWithReducer/AddTask";
 import TaskList from "@/components/profile/stateWithReducer/TaskList";
 
 export default function StateWithReducer() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
 
   function handleAddTask(text) {
-    setTasks([
-      ...tasks,
-      {
-        id: nextId++,
-        text: text,
-        done: false,
-      },
-    ]);
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
   }
 
   function handleChangeTask(task) {
-    setTasks(
-      tasks.map((t) => {
-        if (t.id === task.id) {
-          return task;
-        } else {
-          return t;
-        }
-      }),
-    );
+    dispatch({
+      type: "changed",
+      task: task,
+    });
   }
 
   function handleDeleteTask(taskId) {
-    setTasks(tasks.filter((t) => t.id !== taskId));
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
   }
 
   return (
@@ -52,3 +47,33 @@ const initialTasks = [
   { id: 1, text: "Watch a puppet show", done: false },
   { id: 2, text: "Lennon Wall pic", done: false },
 ];
+
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+}

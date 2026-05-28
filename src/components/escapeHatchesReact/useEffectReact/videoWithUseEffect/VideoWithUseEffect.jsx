040fa8a617ea01e0be651c004c32a7e554495ede
Alexander Prypyshnyi <alexpryp@gmail.com>
2026-05-28 23:29:16 +0300

    ADD | VideoWithUseEffect component

--- src/components/escapeHatchesReact/useEffectReact/videoWithUseEffect/VideoWithUseEffect.jsx
+++ src/components/escapeHatchesReact/useEffectReact/videoWithUseEffect/VideoWithUseEffect.jsx
@@ -5,11 +5,13 @@ function VideoPlayer({ src, isPlaying }) {
 
   useEffect(() => {
     if (isPlaying) {
+      console.log("Calling video.play()");
       ref.current.play();
     } else {
+      console.log("Calling video.pause()");
       ref.current.pause();
     }
-  });
+  }, [isPlaying]);
 
   return <video ref={ref} src={src} loop playsInline />;
 }

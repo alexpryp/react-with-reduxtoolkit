2a3d71cb4312627496e9dbe922f7cefb6cdbf771
Alexander Prypyshnyi <alexpryp@gmail.com>
2026-06-10 07:59:08 +0300

    ADD | FixVariable component

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,7 @@ import FixReconnectionOnKeystrokes from "@/components/escapeHatchesReact/useEffe
 import SwitchSynchronization from "@/components/escapeHatchesReact/useEffectReact/switchSynchronization/SwitchSynchronization";
 import FixConnectionSwitch from "@/components/escapeHatchesReact/useEffectReact/fixConnectionSwitch/FixConnectionSwitch";
 import PopulateChain from "@/components/escapeHatchesReact/useEffectReact/populateChain/PopulateChain";
+import FixVariable from "@/components/escapeHatchesReact/useEffectReact/fixVariable/FixVariable";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -92,6 +93,7 @@ function App() {
         <SwitchSynchronization />
         <FixConnectionSwitch />
         <PopulateChain />
+        <FixVariable />
       </div>
 
       <div>
---
+++ src/components/escapeHatchesReact/useEffectReact/fixVariable/FixVariable.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+export default function FixVariable() {
+  const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(1);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount((c) => c + increment);
+    }, 1000);
+
+    return () => {
+      clearInterval(id);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <hr />
+      <h2>Fix a variable that doesn’t update </h2>
+      <h3>
+        Counter: {count}
+        <button onClick={() => setCount(0)}>Reset</button>
+      </h3>
+      <hr />
+      <p>
+        Every second, increment by:
+        <button
+          disabled={increment === 0}
+          onClick={() => {
+            setIncrement((i) => i - 1);
+          }}
+        >
+          -
+        </button>
+        <b>{increment}</b>
+        <button
+          onClick={() => {
+            setIncrement((i) => i + 1);
+          }}
+        >
+          +
+        </button>
+      </p>
+    </>
+  );
+}

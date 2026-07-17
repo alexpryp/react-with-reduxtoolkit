1b8ed30949b6eb7ac8be3f23e3a7716fc10bcc78
Alexander Prypyshnyi <alexpryp@gmail.com>
2026-07-17 08:30:43 +0300

    REF | move the network connection status check to a custom hook

--- src/components/escapeHatchesReact/customHooks/checkOnline/CheckOnline.jsx
+++ src/components/escapeHatchesReact/customHooks/checkOnline/CheckOnline.jsx
@@ -1,24 +1,7 @@
-import { useState, useEffect } from 'react';
+import useOnlineStatus from '@/components/escapeHatchesReact/customHooks/hooks/useOnlineStatus';
 
 export default function CheckOnline() {
-  const [isOnline, setIsOnline] = useState(true);
-
-  useEffect(() => {
-    function handleOnline() {
-      setIsOnline(true);
-    }
-    function handleOffline() {
-      setIsOnline(false)
-    }
-
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
-
-    return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
-    }
-  }, []);
+  const isOnline = useOnlineStatus();
 
   return (
     <>

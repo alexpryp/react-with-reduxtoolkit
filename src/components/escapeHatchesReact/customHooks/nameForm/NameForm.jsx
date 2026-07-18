d9e85322a27257f96b06339ef849fab99c6f6738
Alexander Prypyshnyi <alexpryp@gmail.com>
2026-07-18 06:06:21 +0300

    REF | move the logic with the state for form fields to an useFormInput custom hook

--- src/components/escapeHatchesReact/customHooks/nameForm/NameForm.jsx
+++ src/components/escapeHatchesReact/customHooks/nameForm/NameForm.jsx
@@ -1,29 +1,22 @@
-import { useState } from 'react';
+import useFormInput from '@/components/escapeHatchesReact/customHooks/hooks/useFormInput';
 
 export default function NameForm() {
-  const [firstName, setFirstName] = useState('Mary');
-  const [lastName, setLastName] = useState('Poppins');
-
-  function handleFirstNameChange(e) {
-    setFirstName(e.target.value);
-  }
-
-  function handleLastNameChange(e) {
-    setLastName(e.target.value);
-  }
+  const firstNameProps = useFormInput('Mary')
+  const lastNameProps = useFormInput('Poppins')
 
   return (
     <>
       <hr></hr>
       <label>
         First name:
-        <input value={firstName} onChange={handleFirstNameChange} />
+        <input {...firstNameProps} />
       </label>
       <label>
         Last name:
-        <input value={lastName} onChange={handleLastNameChange} />
+        <input {...lastNameProps} />
+
       </label>
-      <p><b>Good morning, {firstName} {lastName}.</b></p>
+      <p><b>Good morning, {firstNameProps.value} {lastNameProps.value}.</b></p>
     </>
   )
 }

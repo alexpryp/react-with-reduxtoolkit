72120eb5b7ba6ddbbab23b4bd9dcb72b8d6b7638
Alexander Prypyshnyi <alexpryp@gmail.com>
2026-07-20 14:26:41 +0300

    REF | pass the event handler to custom hook

--- src/components/escapeHatchesReact/customHooks/chatRoom/Room.jsx
+++ src/components/escapeHatchesReact/customHooks/chatRoom/Room.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react';
+import { toast } from "react-toastify";
 
 import { useChatRoom } from '@/components/escapeHatchesReact/customHooks/hooks/useChatRoom';
+
+const message = ({ closeToast, data }) => (
+  <div>
+    {`New message: ${data.msg}`}
+    <button onClick={closeToast}>Close</button>
+  </div>
+);
 
 export default function Room({ roomId }) {
   const [serverUrl, setServerUrl] = useState('https://localhost:1234');
 
   useChatRoom({
     roomId: roomId,
-    serverUrl: serverUrl
+    serverUrl: serverUrl,
+    onReceiveMessage(msg) {
+      toast.success(message, {
+        className: "black-background",
+        progressClassName: "fancy-progress-bar",
+        data: { msg: msg },
+      })
+    }
   });
 
   return (
--- src/components/escapeHatchesReact/customHooks/hooks/useChatRoom.jsx
+++ src/components/escapeHatchesReact/customHooks/hooks/useChatRoom.jsx
@@ -1,16 +1,10 @@
-import { useEffect } from 'react';
-import { toast } from "react-toastify";
+import { useEffect, useEffectEvent } from 'react';
 
 import { createConnection } from '@/components/escapeHatchesReact/customHooks/chatRoom/chat';
 
-const message = ({ closeToast, data }) => (
-  <div>
-    {`New message: ${data.msg}`}
-    <button onClick={closeToast}>Close</button>
-  </div>
-);
+export function useChatRoom({ serverUrl, roomId, onReceiveMessage }) {
+  const onMessage = useEffectEvent(onReceiveMessage);
 
-export function useChatRoom({ serverUrl, roomId }) {
   useEffect(() => {
     const options = {
       serverUrl: serverUrl,
@@ -20,13 +14,9 @@ export function useChatRoom({ serverUrl, roomId }) {
     const connection = createConnection(options);
     connection.connect();
     connection.on('message', (msg) => {
-      toast.success(message, {
-        className: "black-background",
-        progressClassName: "fancy-progress-bar",
-        data: { msg: msg },
-      })
+      onMessage(msg);
     });
 
     return () => connection.disconnect();
-  }, [roomId, serverUrl]);
+  }, [roomId, serverUrl, onMessage]);
 }

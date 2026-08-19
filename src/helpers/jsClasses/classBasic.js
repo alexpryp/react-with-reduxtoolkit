42e312ee5d3384b89cdd626a5d03be8b439f7650
Alexandr <alexpryp@gmail.com>
2026-08-19 10:04:39 +0300

    ADD | Calculator1 function

--- src/helpers/jsClasses/classBasic.js
+++ src/helpers/jsClasses/classBasic.js
@@ -48,31 +48,56 @@ console.log( user1.ref().name );
 // console.log( calculator.sum() );
 // console.log( calculator.mul() );
 
-const ladder = {
-  step: 0,
-  up() {
-    this.step++;
-    return this;
-  },
-  down() {
-    this.step--;
-    return this;
-  },
-  showStep() { // shows the current step
-    console.log( this.step );
-    return this;
-  }
-};
-ladder.up().up().down().showStep().down().showStep();
+// const ladder = {
+//   step: 0,
+//   up() {
+//     this.step++;
+//     return this;
+//   },
+//   down() {
+//     this.step--;
+//     return this;
+//   },
+//   showStep() { // shows the current step
+//     console.log( this.step );
+//     return this;
+//   }
+// };
+// ladder.up().up().down().showStep().down().showStep();
 
-function UserFunc(name) {
-  this.name = name;
-  this.isAdmin = false;
+// function UserFunc(name) {
+//   this.name = name;
+//   this.isAdmin = false;
+// }
+// const user2 = new UserFunc('Jack');
+// console.log(user2.name);
+// console.log(user2.isAdmin);
+// console.log(user2);
+
+const obj = {};
+function A() { return obj; };
+function B() { return obj; };
+const a = new A();
+const b = new B();
+console.log( a == b); // true
+
+function Calculator1() {
+  this.read = function() {
+    this.a = +prompt('Enter value a: ', 0);
+    this.b = +prompt('Enter value b: ', 0);
+  };
+  this.sum = function() {
+    return this.a + this.b;
+  };
+  this.mul = function() {
+    return this.a * this.b;
+  };
 }
-const user2 = new UserFunc('Jack');
-console.log(user2.name);
-console.log(user2.isAdmin);
-console.log(user2);
+const calculator1 = new Calculator1();
+calculator1.read();
+console.log('Sum=' + calculator1.sum());
+console.log('Mul=' + calculator1.mul());
+
 
 
 export class User {
